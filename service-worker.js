a0245d21423fc0904b34d66b8cda86bd7e8aeb56
vgfeeggt5655 <ahmedmohamedelanany@gmail.com>
2025-08-27 00:37:24 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "planx-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/images/icon-192.png",
+  "/images/icon-512.png"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate service worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
