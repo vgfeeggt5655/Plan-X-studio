314f736a593f7fbdabfbed3f9b96312d3c84ae0b
vgfeeggt5655 <ahmedmohamedelanany@gmail.com>
2025-09-06 16:50:14 +0300

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -102,11 +102,6 @@ export const MenuIcon: React.FC<IconProps> = (props) => (
   </svg>
 );
 
-export const XIcon: React.FC<IconProps> = (props) => (
-  <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-  </svg>
-);
 
 export const ArrowUpIcon: React.FC<IconProps> = (props) => (
   <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
